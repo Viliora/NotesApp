3a52c59a2eb92e34e7ee5d93ea1a3c5d82dbca1e
Viliora <144647170+Viliora@users.noreply.github.com>
2024-05-29 14:32:13 +0700

    Update index.js
    clear all line we dont have to used

--- src/script/components/index.js
+++ src/script/components/index.js
@@ -1,16 +1,5 @@
-// import './app-bar.js';
-// import './footer-bar.js';
-
-// import './club-list.js';
-// import './club-item.js';
-
-// import './search-bar.js';
-// import './club-search-error.js';
-
 import './note-form.js';
 import './note-item.js';
 
 import './note-list.js';
-import './notesapp-bar.js';
-
-// import './loading-indicator.js';
+import './loading-indicator.js';
