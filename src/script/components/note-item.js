916c7d647f7c9c4993cd9bb17192409fae362d91
Viliora <144647170+Viliora@users.noreply.github.com>
2024-05-29 14:33:27 +0700

    Update note-item.js

--- src/script/components/note-item.js
+++ src/script/components/note-item.js
@@ -1,57 +1,65 @@
 class NoteItem extends HTMLElement {
-  _shadowRoot = null;
-  // _style = null;
-  // _note = {
-  //       idNote: null,
-  //       title: null,
-  //       body: null,
-  //       createdAt: null,
-  //       archived: null,
-  //     }
-
   constructor() {
     super();
-
     this._shadowRoot = this.attachShadow({ mode: 'open' });
-    // this._style = document.createElement('style');
   }
 
-  _emptyContent() {
-    this._shadowRoot.innerHTML = '';
-}
   connectedCallback() {
+    this.render();
+    // Event listener for delete button
+    this._shadowRoot.querySelector('.button-delete').addEventListener('click', () => {
+      const id = this.getAttribute('id');
+      const event = new CustomEvent('delete-note', {
+        bubbles: true,
+        composed: true,
+        detail: { id: id }
+      });
+      this.dispatchEvent(event);
+    });
+  }
+
+  render() {
     const title = this.getAttribute('title');
     const body = this.getAttribute('body');
-    const createdAt = this.getAttribute('createdAt');
+    const createdAt = this.getAttribute('created-at');
 
     this._shadowRoot.innerHTML = `
       <style>
-        :host {
-          display: block;
-          background: #fff;
-          color: #333;
-          border-radius: 8px;
-          box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.5);
-          padding: 16px;
-          margin-bottom: 20px;
-        }
-        h3 {
-          font-size: 1.5em;
-          margin-bottom: 10px;
-        }
-        p {
-          font-size: 1em;
-          margin-bottom: 10px;
-        }
-        small {
-          font-size: 0.8em;
-          color: #777;
-        }
-      </style>
+      :host {
+        display: block;
+        background: #fff;
+        color: #333;
+        border-radius: 8px;
+        box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.5);
+        padding: 16px;
+        margin-bottom: 20px;
+      }
+      h3 {
+        font-size: 1.5em;
+        margin-bottom: 10px;
+      }
+      p {
+        font-size: 1em;
+        margin-bottom: 10px;
+      }
+      small {
+        font-size: 0.8em;
+        color: #777;
+      }
+      button {
+        background: #f44336;
+        color: white;
+        border: none;
+        padding: 10px;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+    </style>
       <div class="note-item">
         <h3>${title}</h3>
         <p>${body}</p>
         <small>${createdAt}</small>
+        <button type="button" class="button-delete">Delete</button>
       </div>
     `;
   }
