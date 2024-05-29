25e880fd2b43a2dd8a888ae1b686da365cad5832
Viliora <144647170+Viliora@users.noreply.github.com>
2024-05-29 15:07:18 +0700

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public'),
-          to: path.resolve(__dirname, 'dist'),
+          to: path.resolve(__dirname, 'dist/public'),
         },
       ],
     }),
