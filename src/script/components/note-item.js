c9ab8e50a9603a4bd2e056640385278d437657de
Viliora <144647170+Viliora@users.noreply.github.com>
2024-06-01 15:09:50 +0700

    Update note-item.js

--- src/script/components/note-item.js
+++ src/script/components/note-item.js
@@ -24,40 +24,40 @@ class NoteItem extends HTMLElement {
 
     this._shadowRoot.innerHTML = `
       <style>
-      :host {
-        display: block;
-        background: #fff;
-        color: #333;
-        border-radius: 8px;
-        box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.5);
-        padding: 16px;
-        margin-bottom: 20px;
-      }
-      h3 {
-        font-size: 1.5em;
-        margin-bottom: 10px;
-      }
-      p {
-        font-size: 1em;
-        margin-bottom: 10px;
-      }
-      small {
-        font-size: 0.8em;
-        color: #777;
-      }
-      button {
-        background: linear-gradient(#e8dfca, #4f6f52);
-        color: white;
-        border: none;
-        padding: 10px;
-        border-radius: 5px;
-        cursor: pointer;
-      }
-      button:hover{
-        background: #e74c3c;
-      }
-      
-    </style>
+        :host {
+          display: block;
+          background: #fff;
+          color: #333;
+          border-radius: 8px;
+          box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.5);
+          padding: 16px;
+          margin-bottom: 20px;
+        }
+        h3 {
+          font-size: 1.5em;
+          margin-bottom: 10px;
+        }
+        p {
+          font-size: 1em;
+          margin-bottom: 10px;
+        }
+        small {
+          font-size: 0.8em;
+          color: #777;
+        }
+        button {
+          background: linear-gradient(#e8dfca, #4f6f52);
+          color: white;
+          border: none;
+          padding: 10px;
+          border-radius: 5px;
+          cursor: pointer;
+        }
+        button:hover{
+          background: #e74c3c;
+        }
+        
+      </style>
       <div class="note-item">
         <h3>${title}</h3>
         <p>${body}</p>
