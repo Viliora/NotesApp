f7e83071c811c765b5c8191bcfc6f1805c1827a7
Viliora <144647170+Viliora@users.noreply.github.com>
2024-06-01 14:19:20 +0700

    Update notes-api.js
    menghapus fungsi update note dan melakukan beberapa perbaikan

--- src/script/data/remote/notes-api.js
+++ src/script/data/remote/notes-api.js
@@ -1,6 +1,17 @@
 function notesApi() {
   const BASE_URL = "https://notes-api.dicoding.dev/v2";
 
+  const renderAllNotes = (notes) => {
+    const noteListContainerElement = document.querySelector("#notesListContainer");
+    noteListContainerElement.innerHTML = "";
+    const noteListElement = document.createElement('note-list');
+    noteListElement.notes = notes;
+    noteListContainerElement.appendChild(noteListElement);
+    noteListElement.addEventListener('note-removed', (event) => {
+        console.log('Note removed event:', event.detail.id);
+        removeNote(event.detail.id);
+    });
+    
   const getNote = () => {
     fetch(`${BASE_URL}/notes`)
       .then((response) => {
@@ -20,29 +31,6 @@ function notesApi() {
   const insertNote = (note) => {
     fetch(`${BASE_URL}/notes`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(note),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Something went wrong");
-      })
-      .then((responseJson) => {
-        showResponseMessage(responseJson.message);
-        getNote();
-      })
-      .catch((error) => {
-        showResponseMessage(error.message);
-      });
-  };
-
-  const updateNote = (note) => {
-    fetch(`${BASE_URL}/edit/${note.id}`, {
-      method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
@@ -86,10 +74,23 @@ function notesApi() {
     alert(message);
   };
 
+ document.querySelector("#addNoteBtn").addEventListener("click", function () {
+    const titleElement = document.querySelector("#noteTitle");
+    const bodyElement = document.querySelector("#noteBody");
+    if (titleElement.value=='' || bodyElement.value=='') {
+        console.error('Title or body element not found');
+        return;
+    }
+    console.log(titleElement.value)
+    const note = {
+        title: titleElement.value,
+        body: bodyElement.value
+    };
+    insertNote(note)
+    });
   return {
     getNote,
     insertNote,
-    updateNote,
     removeNote,
   };
 }
